62a8abfbec3505f1ba4c7956eb7fe08996983b3e
Zak Abdel-Illah <zak@mqno.dev>
2025-07-03 18:02:49 +0100

    missing assignment on flag pull

--- wrapper.sh
+++ wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # FULL_ARGS=$( echo "$@" | base64 -d | gunzip )
-$(curl "${FFMPEG_METADATA_ENDPOINT}" | jq -r '.flags' | base64 -d | gunzip)
+FULL_ARGS=$(curl "${FFMPEG_METADATA_ENDPOINT}" | jq -r '.flags' | base64 -d | gunzip)
 set -- $FULL_ARGS
 FULL_ARGS=( "$@" )
 
