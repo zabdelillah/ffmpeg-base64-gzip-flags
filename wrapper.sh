149300746086968fc3bd7dafbc9f7c4b036f122a
Zak Abdel-Illah <zak@mqno.dev>
2025-09-18 04:39:04 +0100

    overlays: basic integration

--- wrapper.sh
+++ wrapper.sh
@@ -152,9 +152,12 @@ echo "$FFMPEG_OVERLAYS_CMD" | grep -oP "overlay=[a-z\=\'\(\),\d\*\.\:]+\[out[\d]
   END=$(echo $line | grep -oP 'lte\(t\,[\d]+\)' | grep -oP '[0-9\.]+')  
   NESTED_FILTERS=$(echo $line | grep -oP 'x=[\d\:a-z\=]+')
   NEW_FILTERS="[0:v][1:v]overlay=${NESTED_FILTERS}[out]"
-  DURATION=$((END - START))
+  DURATION=$(awk -v start="$START" -v end="$END" 'BEGIN {print end - start}')
   echo "[OVERLAY${INDEX}] ffmpeg -nostdin -progress /dev/stderr -i ${file_inputs_b[(($INDEX-1))]} -i ${file_inputs_b[$INDEX]} -filter_complex ${NEW_FILTERS} -map '[out]' -t ${DURATION} ${file_inputs_b[$INDEX]}.overlay.mp4"
   ffmpeg -nostdin -progress /dev/stderr -i ${file_inputs_b[(($INDEX-1))]} -i ${file_inputs_b[$INDEX]} -filter_complex "${NEW_FILTERS}" -map '[out]' -t ${DURATION} ${file_inputs_b[$INDEX]}.overlay.mp4 -y 2> >(sed "s/^/[OVERLAY${INDEX}] /")
+  if (( INDEX == 1 )); then
+    echo "file '${file_inputs_b[$INDEX]}.overlay.mp4'" >> concat.txt
+  fi
 done
 # End extract overlay lines
 
